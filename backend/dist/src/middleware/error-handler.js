5dbc277123b4d9e4c3eebc6dc90a7378040af8d9
Mindaugas <mindaugas@Mindaugass-MacBook-Pro.local>
2020-04-19 23:51:14 +0300

    fixing .gitignore

--- backend/dist/src/middleware/error-handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (err, req, res, next) => {
-    const status = err.statusCode || 500;
-    const message = err.message;
-    const data = err.data;
-    res.status(status).json({ message, data });
-};
--- backend/dist/src/routes/portal.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const router = express_1.Router();
-router.get('/', controllers_1.PortalController.getSupportedPortalsAsFilters);
-router.post('/', controllers_1.PortalController.addPortal);
-exports.default = router;
