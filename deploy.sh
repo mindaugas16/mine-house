aa4b4d6a8dba81877fac3e4786915c8cea258131
Mindaugas <morkunas1996@gmail.com>
2021-02-16 14:00:06 +0200

    Push build into docker hub

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# Build and tag image
+docker-compose build --pull
+# Log in to Docker Hub and push
+
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+docker-compose push
